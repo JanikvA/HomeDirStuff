8334c8079312badcf0ae187fa61038873a7ef14c
JanikvA <j.v.ahnen@gmx.de>
2018-11-13 11:23:53 +0100

    update

---
+++ HomeDir/cpFiles.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env sh
+cp ~/.vimrc vimrc
+cp ~/.bashrc bashrc
+cp ~/.inputrc inputrc
+cp ~/.rootrc rootrc
+cp ~/rootlogon.C rootlogon.C 
+cp ~/rootlogoff.C rootlogoff.C  
+cp ~/.rootalias.C rootalias.C  
